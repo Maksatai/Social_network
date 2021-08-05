df1e64b669eb9d38bbb7c4b3f543031af0979f45
Maksatai <erkbvna@gmail.com>
2021-08-06 00:39:07 +0600

    changed log in page and sign up page

--- social_network/settings.py
+++ social_network/settings.py
@@ -136,3 +136,4 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+LOGOUT_REDIRECT_URL = 'home'
--- social_network/urls.py
+++ social_network/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('accounts/',include('log_in.urls')),
     path('accounts/',include('sign_up.urls')),
     path('',include('core.urls')),
+    path('accounts/',include('django.contrib.auth.urls')),
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
