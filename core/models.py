1e9f15f7e2e85fc6126f48202b57b920e787fc27
zhanyll <janylayka95@gmail.com>
2021-08-10 16:46:43 +0600

    added model User

--- core/models.py
+++ core/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    login = models.ForeignKey(to=User, on_delete=models.SET_NULL, null=True, blank=True)
+    name = models.CharField(max_length=255)
+    surname = models.CharField(max_length=255)
+    age = models.IntegerField(max=100, min=10)      
+    email = models.CharField(max_length=255)
+    decription = models.TextField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    friends = models.IntegerField(default=0)
+    is_online = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.login
+
+    class Meta:
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
+        ordering = ['name']
