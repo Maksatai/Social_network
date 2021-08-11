ed585530e0693b55faa510856955d9cebaf34b04
zhanyll <janylayka95@gmail.com>
2021-08-11 15:58:55 +0600

    added Post model

--- core/models.py
+++ core/models.py
@@ -2,21 +2,19 @@ from django.db import models
 
 # Create your models here.
 
-class User(models.Model):
-    login = models.ForeignKey(to=User, on_delete=models.SET_NULL, null=True, blank=True)
+class Post(models.Model):
     name = models.CharField(max_length=255)
     surname = models.CharField(max_length=255)
-    age = models.IntegerField(max=100, min=10)      
-    email = models.CharField(max_length=255)
-    decription = models.TextField(null=True, blank=True)
+    title = models.CharField(max_length=255)
+    text = models.TextField(null=True, blank=True)
+    photo = models.ImageField(null=True, blank=True)
+    video = models.FileField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    friends = models.IntegerField(default=0)
-    is_online = models.BooleanField(default=False)
 
     def __str__(self):
-        return self.login
+        return self.title
 
     class Meta:
-        verbose_name = 'User'
-        verbose_name_plural = 'Users'
-        ordering = ['name']
+        verbose_name = 'Post'
+        verbose_name_plural = 'Posts'
+        ordering = ['created_at']
