5f8ec33d9046a3941bfdd49b6ede76d208acdfc1
zhanyll <janylayka95@gmail.com>
2021-08-24 22:48:40 +0600

    updated Post model

--- post/models.py
+++ post/models.py
@@ -1,19 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
 # Create your models here.
 
 class Post(models.Model):
-    author = models.CharField(max_length=255)
-    title = models.CharField(max_length=255)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     text = models.TextField(null=True, blank=True)
-    photo = models.ImageField(null=True, blank=True)
-    video = models.FileField(null=True, blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
+    photo = models.ImageField(upload_to='will/create/later', null=True, blank=True)
+    video = models.FileField(upload_to='will/create/later', null=True, blank=True)
+    created_at = models.DateTimeField(default=timezone.now)
+    tags = models.CharField(max_length=100, blank=True)
 
     def __str__(self):
-        return self.title
+        return self.text
 
     class Meta:
         verbose_name = 'Post'
         verbose_name_plural = 'Posts'
-        ordering = ['created_at']
