b75e6c35547f7b1dbf41243af8aaab136f71f418
zhanyll <janylayka95@gmail.com>
2021-08-24 23:19:20 +0600

    added PostListView view

--- core/views.py
+++ core/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from post.models import Post, Like
+from django.views.generic import ListView
 
-class HomeView(TemplateView):
-    template_name = 'base.html'
+# class HomeView(TemplateView):
+#     template_name = 'base.html'
 
-def homepage(request):
-    return render(request, 'base.html')
+# def homepage(request):
+#     return render(request, 'base.html')
+
+class PostListView(ListView):
+	model = Post
+	template_name = 'base.html'
+	context_object_name = 'posts'
+	ordering = ['-created_at']
+	paginate_by = 12
+	def get_context_data(self, **kwargs):
+		context = super(PostListView, self).get_context_data(**kwargs)
+		if self.request.user.is_authenticated:
+			liked = [i for i in Post.objects.all() if Like.objects.filter(user = self.request.user, post=i)]
+			context['liked_post'] = liked
+		return context
--- post/models.py
+++ post/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
+from django.urls import reverse
 
 # Create your models here.
 
@@ -15,6 +16,14 @@ class Post(models.Model):
     def __str__(self):
         return self.text
 
+    # def get_absolute_url(self):
+	# 	return reverse('post-detail', kwargs={'pk': self.pk})
+  
     class Meta:
         verbose_name = 'Post'
         verbose_name_plural = 'Posts'
+
+
+class Like(models.Model):
+	user = models.ForeignKey(User, related_name='likes', on_delete=models.CASCADE)
+	post = models.ForeignKey(Post, related_name='likes', on_delete=models.CASCADE)
