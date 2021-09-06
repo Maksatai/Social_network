9773072bc27b08a9a54b3c05bdba0bd75a82b4b2
zhanyll <janylayka95@gmail.com>
2021-09-06 12:21:06 +0600

    some changes for merge request

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from core.views import HomeView
+from core.views import HomeView, PostListView
 
 urlpatterns = [
     path('', HomeView.as_view(), name="homepage"),
+    path('feed/', PostListView.as_view(), name='feed')
 ]
--- core/views.py
+++ core/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from post.models import Post, Like
-from django.views.generic import ListView
+from django.views.generic import ListView, TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-# class HomeView(TemplateView):
-#     template_name = 'base.html'
+class HomeView(TemplateView):
+    template_name = 'base.html'
 
 # def homepage(request):
 #     return render(request, 'base.html')
@@ -20,3 +21,21 @@ class PostListView(ListView):
 			liked = [i for i in Post.objects.all() if Like.objects.filter(user = self.request.user, post=i)]
 			context['liked_post'] = liked
 		return context
+
+
+class UserPostListView(LoginRequiredMixin, ListView):
+	model = Post
+	template_name = 'core/user_posts.html'
+	context_object_name = 'posts'
+	paginate_by = 12
+
+	def get_context_data(self, **kwargs):
+		context = super(UserPostListView, self).get_context_data(**kwargs)
+		user = get_object_or_404(User, username=self.kwargs.get('username'))
+		liked = [i for i in Post.objects.filter(user_name=user) if Like.objects.filter(user = self.request.user, post=i)]
+		context['liked_post'] = liked
+		return context
+
+	def get_queryset(self):
+		user = get_object_or_404(User, username=self.kwargs.get('username'))
+		return Post.objects.filter(user_name=user).order_by('-date_posted')
