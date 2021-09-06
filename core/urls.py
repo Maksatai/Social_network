d4d877550de7db4a23abbcca56d19765f8eab1ef
zhanyll <janylayka95@gmail.com>
2021-09-06 18:25:54 +0600

    bugs fixes

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,9 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 from . import views
 from .views import HomeView, PostListView
 
 urlpatterns = [
     path('', PostListView.as_view(), name="homepage"),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
