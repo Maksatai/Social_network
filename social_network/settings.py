30620d0f8d1694a5c45652c51d808dcdf395877a
zhanyll <janylayka95@gmail.com>
2021-09-28 19:28:54 +0600

    modified settings for pythonanywhere

--- social_network/settings.py
+++ social_network/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-z#n+6+rwo(=#b9e73-qr9e&&h_#pqu&lofs#nbi4mv-2lyq_0m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
